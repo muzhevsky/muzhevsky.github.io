79e531d1faba1624a0b37c6f6b09bf6023cd2376
andrey_bb <andreimuzhevsky@gmail.com>
2022-07-18 22:21:19 +0400

    main idea is almost implemented

--- main.js
+++ main.js
@@ -1,16 +1,75 @@
 
     var array = [];
     var isLoaded = false;
-    var currentLetter = "а";
     var currentLetterIndex = 0;
     var rightCounter = 0;
     
     var container = document.getElementsByClassName("inputContainer")[0];
+    var buttonContainer = document.getElementsByClassName("buttonsContainer")[0];
     var statCPM = document.getElementsByClassName("statCPM")[0];
     var statAccuracy = document.getElementsByClassName("statAccuracy")[0];
     
-    document.addEventListener("DOMContentLoaded", HttpGetAsync("https://muzhevsky.github.io/russian_dictionary/", GetWordsCallback));
+    document.addEventListener("DOMContentLoaded", function(){
+        HttpGetAsync("https://muzhevsky.github.io/russian_dictionary/", GetWordsCallback);
+        GenerateButtons();
 
+        let time = 0;
+        let delayArray = [];
+
+        document.addEventListener("keydown",function(e){
+            if(isLoaded){
+                if(currentLetterIndex >= container.children.length-1) return;
+    
+                if(e.code == "ControlLeft" || e.code == "ShiftLeft" || e.code == "AltLeft" || e.code == "CapsLock") return;
+        
+                if(e.key == container.children[currentLetterIndex].innerHTML[0]){
+                    container.children[currentLetterIndex].style.backgroundColor = "green";
+                    currentLetterIndex++;
+                    rightCounter++;
+                }
+    
+                else{
+                    container.children[currentLetterIndex].style.backgroundColor = "red";
+                    currentLetterIndex++;
+                }
+                
+                if (time == 0){
+                    time = performance.now();
+                }
+    
+                else{
+                    delayArray.push(performance.now() - time);
+                    time = performance.now();
+        
+                    let sum = 0;
+                    let average = 0;
+        
+                    for(let i = 0; i < delayArray.length; i++) 
+                        sum+=delayArray[i];
+                    average = sum/delayArray.length;
+        
+                    statCPM.innerHTML = Math.round(60000/average);
+                    statAccuracy.innerHTML = Math.round(rightCounter/currentLetterIndex*100);
+                }
+            }
+        });
+    });
+    function GenerateButtons(){
+        let letters = "абвгдежзийклмнопрстуфхцчшщьыъэюя";
+        letters = Array.from(letters);
+        letters.forEach(function(element){
+            buttonContainer.innerHTML+="<span class = 'letterButton'>"+element+"</span>";
+        });
+        let buttons = Array.from(document.getElementsByClassName("letterButton"));
+        
+        buttons.forEach(function(element){
+            console.log(element);
+            element.addEventListener("click", function() {
+                let words = GetWordsWithCurrentLetter(element.textContent);
+                PushWordsIntoContainer(words);
+            });
+        });
+    }
     function isAWord(string){
         return (string.length > 5 && string.length < 12);
     }
@@ -21,7 +80,7 @@
         array = array.filter(isAWord);
         if (array.length > 0){
             isLoaded = true;
-            PushWordsIntoContainer(GetWordsWithCurrentLetter());
+            PushWordsIntoContainer(GetWordsWithCurrentLetter("а"));
         } 
     
         else HttpGetAsync("https://muzhevsky.github.io/russian_dictionary/", GetWordsCallback);
@@ -42,7 +101,7 @@
     
     let test = "asd";
     
-    function GetWordsWithCurrentLetter(){
+    function GetWordsWithCurrentLetter(currentLetter){
         result = [];
         for(let i = 0; i < 20; i++){
     
@@ -69,47 +128,14 @@
     }
     
     function PushWordsIntoContainer(words){
-        let time = 0;
-        let delayArray = [];
+        currentLetterIndex = 0;
+        rightCounter = 0;
+        container.innerHTML = "";
         words.forEach(function(item){
             for(let i = 0; i < item.length; i++){
                 container.innerHTML+="<span>"+item[i]+"</span>";
             }
             container.innerHTML+="<span> </span>";
         });
-        
-        document.addEventListener("keydown",function(e){
-    
-            if(currentLetterIndex >= container.children.length-1) return;
-    
-            if(e.code == "ControlLeft" || e.code == "ShiftLeft" || e.code == "AltLeft" || e.code == "CapsLock") return;
-    
-            if(e.key == container.children[currentLetterIndex].innerHTML[0]){
-                container.children[currentLetterIndex].style.backgroundColor = "green";
-                currentLetterIndex++;
-                rightCounter++;
-            }
-            else{
-                container.children[currentLetterIndex].style.backgroundColor = "red";
-                currentLetterIndex++;
-            }
-            
-            if(time == 0){
-                time = performance.now();
-            }
-            else{
-                delayArray.push(performance.now() - time);
-                time = performance.now();
-    
-                let sum = 0;
-                let average = 0;
-    
-                for(let i = 0; i < delayArray.length; i++) 
-                    sum+=delayArray[i];
-                average = sum/delayArray.length;
-    
-                statCPM.innerHTML = Math.round(60000/average);
-                statAccuracy.innerHTML = Math.round(rightCounter/currentLetterIndex*100);
-            }
-        });
+        isLoaded = true;
     }
